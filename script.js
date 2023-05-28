552d23ba0c4110a155358f7cf480177b7174ef6a
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-28 09:05:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let userAgent = navigator.userAgent;
+
+let regex = /(.*?)\/(.*?)\s(.*)/;
+
+let match = regex.exec(userAgent);
+
+let browserName = match[1];
+let version = match[2];
+
+// let div = document.createElement('div');
+
+// div.innerHTML =  "You are using " + browserName + " version " + version;
+
+let browserInfo = document.getElementById("browser-info");
+
+browserInfo.innerHTML = "You are using " + browserName + " version " + version;
